d331912d975d3b7cbdd541376a9dde6393a300ae
vairavanrams <vairavanrams1992@gmail.com>
2019-05-16 20:12:49 +0530

    new modified git class

--- src/org/test/Gitnew1.java
+++ src/org/test/Gitnew1.java
@@ -5,6 +5,7 @@ public class Gitnew1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is my github");
+		System.out.println("This is my new line");
 
 	}
 
